963ef090fe7e9435bb59a51a0facfb9a3119943d
ChiaraCati <chiara.catizone@studio.unibo.it>
2022-06-08 10:42:28 +0200

    c

---
+++ WU-logs.js
@@ -0,0 +1,21 @@
+trials =  0
+removed1 =  0
+removed2 =  0
+removed3 =  0
+removed4 =  0
+removed5 =  0
+
+// how many times did the user repeat WU?
+function Trials() {
+    trials = trials+1
+    console.log(trials)
+
+};
+
+
+//How many times did he change his mind on the order? WHich are the most removed positions?
+
+function Removed (ev){
+    console.log('hey')
+    
+}
