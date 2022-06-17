40ff4d9df083c5dac930841fb9a9493d4669f921
ChiaraCati <chiara.catizone@studio.unibo.it>
2022-06-17 14:36:57 +0200

    c

--- task_logs.js
+++ task_logs.js
@@ -1,10 +1,11 @@
 var start = new Date();
-
-var btn = document.querySelector('#twofuns');
-btn.addEventListener('click',method1);
-btn.addEventListener('click',method2);
-function method2(){
-  console.log("Method 2");
+var n_queries = 0;
+console.log("Query number: ", n_queries);
+function addQuery(){
+    n_queries+=1
+    console.log("Add query");
+    sessionStorage.setItem('n_queries', n_queries);
+    console.log('n_queries =', n_queries)
 }
 
 console.log('Start time: ' + start);
