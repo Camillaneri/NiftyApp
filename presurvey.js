8ae0bdf949a3ff14a8c831c6396c8775c83622b6
ChiaraCati <chiara.catizone@studio.unibo.it>
2022-06-17 11:15:36 +0200

    c

--- presurvey.js
+++ presurvey.js
@@ -1,5 +1,6 @@
 $(document).ready(() => {
     $("input[type=checkbox]").change(onFormUpdate);
+    $("input[type=radio]").change(onFormUpdate);
     
   })
   
@@ -8,8 +9,10 @@ $(document).ready(() => {
     const ArtExp = $("#ArtExp1").prop('checked') || $("#ArtExp2").prop('checked') || $("#ArtExp3").prop('checked') || $("#ArtExp4").prop('checked') || $("#ArtExp5").prop('checked');
     const NftExp = $("#NftExp1").prop('checked') || $("#NftExp2").prop('checked') || $("#NftExp3").prop('checked') || $("#NftExp4").prop('checked') || $("#NftExp5").prop('checked');
     const FinBcExp = $("#FinBcExp1").prop('checked') || $("#FinBcExp2").prop('checked') || $("#FinBcExp3").prop('checked') || $("#FinBcExp4").prop('checked') || $("#FinBcExp5").prop('checked');
-    console.log(ArtExp, NftExp, FinBcExp)
-    if (ArtExp  && NftExp && FinBcExp) {
+    const useMM = $("#useMM1").prop('checked') || $("#useMM2").prop('checked') || $("#useMM3").prop('checked') || $("#useMM4").prop('checked') || $("#useMM5").prop('checked');
+
+    console.log(ArtExp, NftExp, FinBcExp, useMM)
+    if (ArtExp  && NftExp && FinBcExp && useMM) {
       $("#submit").removeAttr("disabled");
     } else {
       $("#submit").attr("disabled", "disabled");
