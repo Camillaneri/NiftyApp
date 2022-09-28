c45d1a6ec0c89e9c819b0799faab07fc110edd4d
ChiaraCati <chiara.catizone@studio.unibo.it>
2022-09-28 15:45:21 +0200

    up docu reordee

--- js_app/index.js
+++ js_app/index.js
@@ -1,25 +1,20 @@
+// clear old session storage if user restarts
 sessionStorage.clear();
+
 var WUround = 0
 sessionStorage.setItem('WUround', WUround)
+
+// function for class reveal in HTML files 
+// MAGARI DA COMMENTARE CHE FA 
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
-  
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
       
       elementVisible = 150;
-      //console.log("reveals[i] "+reveals[i].outerHTML)
-      // console.log("elementTop "+elementTop)
-      // console.log("windowHeight "+windowHeight)
-      // console.log("elementVisible "+elementVisible)
-      // console.log("windowHeight - elementVisible "+(windowHeight - elementVisible))
-      
-
       if (reveals[i].id == "goonbutton"){
-        //console.log("ecco")
         elementVisible = 5;
-        //console.log(elementVisible)
       }
       if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("active");
